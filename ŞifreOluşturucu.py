5c756eee6d39f1ea29f0e048be5edf6ae0eeeafd
yakupemreyerlii <yakupemreoyunhesap@gmail.com>
2024-10-24 22:21:47 +0300

    Şifre Oluşturucu

---
+++ ŞifreOluşturucu.py
@@ -0,0 +1,10 @@
+import random
+
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+parola_uzunlugu = int(input("Parolanın uzunluğunu girin: "))
+olusturulan_parola = ""
+
+for i in range(parola_uzunlugu):
+    olusturulan_parola += random.choice(karakterler)
+
+print("Oluşturulan Parola:", olusturulan_parola)
