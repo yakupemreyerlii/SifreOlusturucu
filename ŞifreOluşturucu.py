bd515f12c707d6abf0ff2e58296230ff28ec8c9c
yakupemreyerlii <yakupemreoyunhesap@gmail.com>
2024-11-07 22:18:54 +0300

    7.11.2024 ŞifreOluşturucu.py
    Yeni Notlar Eklendi

--- ŞifreOluşturucu.py
+++ ŞifreOluşturucu.py
@@ -2,10 +2,14 @@ import random
 
 #KULLANACAĞI KARAKTERLER
 karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+#Uzunluk Alınıyor
 parola_uzunlugu = int(input("Parolanın uzunluğunu girin: "))
+
 olusturulan_parola = ""
+
 
 for i in range(parola_uzunlugu):
     olusturulan_parola += random.choice(karakterler)
 
+#Kod Çıktısı Veriliyor
 print("Oluşturulan Parola:", olusturulan_parola)
